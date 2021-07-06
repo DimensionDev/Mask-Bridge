69a10f357d9cecb04d167d6b4dbce683c171a7a7
UncleBill <unclebill@mindhand.io>
2021-07-06 23:19:59 +0800

    fix: add source of this fork and forum link (#33)

--- packages/dapp/src/components/common/Footer.jsx
+++ packages/dapp/src/components/common/Footer.jsx
@@ -9,59 +9,73 @@ import { Link } from 'react-router-dom';
 export const Footer = () => {
   const smallScreen = useBreakpointValue({ base: true, sm: false });
   return (
-    <Flex
-      position="relative"
-      justify={{ base: 'center', sm: 'space-between' }}
-      align="center"
-      h={20}
-      maxW="75rem"
-      px={8}
-      w="100%"
-      color="grey"
-    >
-      {!smallScreen && (
-        <Link to="/" display={{ base: 'none', sm: 'block' }}>
-          <Text color="darkblue">&copy; 2021 Mask Network</Text>
-        </Link>
-      )}
-      <HStack spacing={4}>
-        <Box _hover={{ color: 'blue.500' }} transition="0.25s">
-          <a
-            href="https://masknetwork.medium.com/"
-            rel="noreferrer noopener"
-            target="_blank"
-          >
-            <MediumIcon />
-          </a>
-        </Box>
-        <Box _hover={{ color: 'blue.500' }} transition="0.25s">
-          <a
-            href="https://t.me/maskbook_group"
-            rel="noreferrer noopener"
-            target="_blank"
-          >
-            <TelegramIcon />
-          </a>
-        </Box>
-        <Box _hover={{ color: 'blue.500' }} transition="0.25s">
-          <a
-            href="https://github.com/DimensionDev/Maskbook"
-            rel="noreferrer noopener"
-            target="_blank"
-          >
-            <GithubIcon />
-          </a>
-        </Box>
-        <Box _hover={{ color: 'blue.500' }} transition="0.25s">
-          <a
-            href="https://twitter.com/realmaskbook"
-            rel="noreferrer noopener"
-            target="_blank"
-          >
-            <TwitterIcon />
-          </a>
-        </Box>
-      </HStack>
-    </Flex>
+    <>
+      <Flex
+        position="relative"
+        justify={{ base: 'center', sm: 'space-between' }}
+        align="center"
+        maxW="75rem"
+        px={8}
+        w="100%"
+        color="grey"
+      >
+        {!smallScreen && (
+          <Link to="/" display={{ base: 'none', sm: 'block' }}>
+            <Text color="darkblue">&copy; 2021 Mask Network</Text>
+          </Link>
+        )}
+        <HStack spacing={4}>
+          <Box _hover={{ color: 'blue.500' }} transition="0.25s">
+            <a
+              href="https://masknetwork.medium.com/"
+              rel="noreferrer noopener"
+              target="_blank"
+            >
+              <MediumIcon />
+            </a>
+          </Box>
+          <Box _hover={{ color: 'blue.500' }} transition="0.25s">
+            <a
+              href="https://t.me/maskbook_group"
+              rel="noreferrer noopener"
+              target="_blank"
+            >
+              <TelegramIcon />
+            </a>
+          </Box>
+          <Box _hover={{ color: 'blue.500' }} transition="0.25s">
+            <a
+              href="https://github.com/DimensionDev/Maskbook"
+              rel="noreferrer noopener"
+              target="_blank"
+            >
+              <GithubIcon />
+            </a>
+          </Box>
+          <Box _hover={{ color: 'blue.500' }} transition="0.25s">
+            <a
+              href="https://twitter.com/realmaskbook"
+              rel="noreferrer noopener"
+              target="_blank"
+            >
+              <TwitterIcon />
+            </a>
+          </Box>
+        </HStack>
+      </Flex>
+      <Flex w="100%" maxW="75rem" px={8} h={10} pt={1} pb={15}>
+        <Text
+          color="#7A83B2"
+          fontSize="sm"
+          w="100%"
+          textAlign={smallScreen ? 'center' : 'inherit'}
+        >
+          Forked from{' '}
+          <Link href="https://omni.xdaichain.com/" rel="noopener noreferrer">
+            OmniBridge
+          </Link>
+        </Text>
+      </Flex>
+    </>
   );
 };

f257bf7d547f0f7f9fbec465f9aa7275d0ab155d
unclebill <billbill290@gmail.com>
2021-07-08 12:16:13 +0800

    feat: add matic bridge link

--- packages/dapp/src/components/common/BridgeDropdown.jsx
+++ packages/dapp/src/components/common/BridgeDropdown.jsx
@@ -1,9 +1,17 @@
 import {
   Button,
+  Link,
   Menu,
   MenuButton,
   MenuItem,
   MenuList,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
   Text,
   useBreakpointValue,
 } from '@chakra-ui/react';
@@ -11,11 +19,13 @@ import { useSettings } from 'contexts/SettingsContext';
 import { DownArrowIcon } from 'icons/DownArrowIcon';
 import { NetworkIcon } from 'icons/NetworkIcon';
 import { networks } from 'lib/networks';
-import React, { useCallback, useEffect } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
+const MATIC_BRIDGE_LINK = 'https://wallet.matic.network/bridge';
 export const BridgeDropdown = ({ close = () => {} }) => {
   const { bridgeDirection, setBridgeDirection } = useSettings();
   const placement = useBreakpointValue({ base: 'top', md: 'top-end' });
+  const [modalVisible, setModalVisible] = useState(false);
 
   const setItem = useCallback(
     e => {
@@ -39,35 +49,101 @@ export const BridgeDropdown = ({ close = () => {} }) => {
   }, [isValidNetwork, networkOptions, setBridgeDirection]);
 
   return (
-    <Menu placement={placement}>
-      <MenuButton
-        as={Button}
-        leftIcon={<NetworkIcon />}
-        rightIcon={<DownArrowIcon boxSize="0.5rem" color="black" />}
-        color="grey"
-        bg="none"
-        _hover={{ color: 'blue.500', bgColor: 'blackAlpha.100' }}
-        p={2}
-      >
-        <Text color="black" textTransform="uppercase" fontSize="sm">
-          {networks[currentBridgeDirection].label}
-        </Text>
-      </MenuButton>
-      <MenuList border="none" boxShadow="0 0.5rem 1rem #CADAEF" zIndex="3">
-        {Object.entries(networks).map(([key, { label }]) => (
+    <>
+      <Menu placement={placement}>
+        <MenuButton
+          as={Button}
+          leftIcon={<NetworkIcon />}
+          rightIcon={<DownArrowIcon boxSize="0.5rem" color="black" />}
+          color="grey"
+          bg="none"
+          _hover={{ color: 'blue.500', bgColor: 'blackAlpha.100' }}
+          p={2}
+        >
+          <Text color="black" textTransform="uppercase" fontSize="sm">
+            {networks[currentBridgeDirection].label}
+          </Text>
+        </MenuButton>
+        <MenuList border="none" boxShadow="0 0.5rem 1rem #CADAEF" zIndex="3">
+          {Object.entries(networks).map(([key, { label }]) => (
+            <MenuItem
+              value={key}
+              onClick={setItem}
+              key={key}
+              textTransform="uppercase"
+              fontWeight="700"
+              fontSize="sm"
+              justifyContent="center"
+            >
+              {label}
+            </MenuItem>
+          ))}
           <MenuItem
-            value={key}
-            onClick={setItem}
-            key={key}
+            key="eth-matic"
             textTransform="uppercase"
             fontWeight="700"
             fontSize="sm"
             justifyContent="center"
+            onClick={() => {
+              setModalVisible(true);
+            }}
           >
-            {label}
+            ETH-Matic
           </MenuItem>
-        ))}
-      </MenuList>
-    </Menu>
+        </MenuList>
+      </Menu>
+
+      <Modal
+        isOpen={modalVisible}
+        onClose={() => setModalVisible(false)}
+        closeOnEsc
+        closeOnOverlayClick
+        isCentered
+      >
+        <ModalOverlay background="modalBG">
+          <ModalContent
+            background="white"
+            borderRadius="20px"
+            w="448px"
+            boxSizing="border-box"
+          >
+            <ModalHeader height="40px" fontSize={32} color="#1A1A76">
+              Confirm
+            </ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              <Text color="#7B8192" fontSize={18}>
+                Confirm to enter the website for bridging assets provided by
+                Matic/Polygon Network{' '}
+                <Link
+                  href={MATIC_BRIDGE_LINK}
+                  rel="noopener noreferrer"
+                  target="_blank"
+                  color="#2b3bbc"
+                >
+                  {MATIC_BRIDGE_LINK}
+                </Link>
+              </Text>
+            </ModalBody>
+            <ModalFooter>
+              <Button
+                colorScheme="blue"
+                bg="primary"
+                color="white"
+                isFullWidth
+                borderRadius="20px"
+                onClick={() => {
+                  const newTab = window.open();
+                  newTab.location = MATIC_BRIDGE_LINK;
+                  setModalVisible(false);
+                }}
+              >
+                Confirm
+              </Button>
+            </ModalFooter>
+          </ModalContent>
+        </ModalOverlay>
+      </Modal>
+    </>
   );
 };
